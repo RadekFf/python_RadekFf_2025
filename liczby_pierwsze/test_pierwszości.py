8eab220fd7132e1b1f2cd704038e418b8d5b57d1
Radoslaw Filozof <radekff77@gmail.com>
2025-03-22 13:07:33 +0100

    test pierwszości, wersja2

--- liczby_pierwsze/test_pierwszości.py
+++ liczby_pierwsze/test_pierwszości.py
@@ -1,9 +1,13 @@
 def czy_pierwsza(liczba : int):
-    liczba = 50
-    for i in range (2, liczba):
+    if liczba < 2:
+        return False
+
+    for i in range (2, int(liczba ** 0.5) + 1):
         if liczba % i == 0:
             return False
         return True
 
 if __name__ == '__main__':
-    print(czy_pierwsza(50))
+    liczba = int(input())
+    wynik = czy_pierwsza(liczba)
+    print(wynik)
