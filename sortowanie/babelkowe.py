40893c60f2bfa52ee2e7ecb93d6b8eeccd62b22a
Radoslaw Filozof <radekff77@gmail.com>
2025-03-22 12:25:55 +0100

    wywoływanie sortowania z innego pliku

--- sortowanie/babelkowe.py
+++ sortowanie/babelkowe.py
@@ -1,8 +1,12 @@
-dane = [2,5,6,3,1]
-n = len(dane)
-for i in range (0, n-1):
-    for j in range (0, n-1-i):
-        if dane[j] > dane[j+1]:
-           dane[j], dane[j+1] = dane[j+1], dane[j]
+def sortuj_babelkowe(dane):
+    n = len(dane)
 
-print(dane)
+    for i in range (0, n-1):
+        for j in range (0, n-1-i):
+            if dane[j] > dane[j+1]:
+               dane[j], dane[j+1] = dane[j+1], dane[j]
+
+if __name__ == '__main__':
+    dane = [5,78,2,56,5,26]
+    sortuj_babelkowe(dane)
+    print(dane)
