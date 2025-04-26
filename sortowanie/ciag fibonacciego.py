c511f8c05d3cc2588bfee8be48cb05ac1cc55895
Radoslaw Filozof <radekff77@gmail.com>
2025-04-26 14:31:23 +0200

    fibonacci1

--- sortowanie/ciag fibonacciego.py
+++ sortowanie/ciag fibonacciego.py
@@ -1,17 +1,10 @@
-def f_rekurecyjny(n):
-    return f_rekurecyjny(n-1) + f_rekurecyjny(n-2)
-def f_rekuracyjny(n):
-    if n<2:
-        return n
-   fn_1 = 1
-   fn_2 = 0
-for _ in range (2, n+1):
-    fn = fn_1 + fn_2
-    fn_2 = fn_1
-    fn_1 = fn
-return fn
-fn_2, fn_1 = fn_1, fn_2 + fn_1
-return fn_1
+def f_rekurencja_ogonowa (n, fn=0, fn_1=1):
+    if n == 0:
+        return fn_2
+    elif n == 1:
+        return fn_1
+    else:
+        return f_rekurencja_ogonowa(n, n-1, fn_1, fn_2 + fn_1)
 
 if __name__ == '__main__':
-print (f_iteracyjny(40)
+    print(f_rekurencja_ogonowa(50))
